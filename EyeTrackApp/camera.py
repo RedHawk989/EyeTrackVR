f23347d5007adc458e82316f5a279dc5f68ba0bc
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2022-07-08 11:30:34 -0700

    fixes wireless streaming

--- EyeTrackApp/camera.py
+++ EyeTrackApp/camera.py
@@ -40,9 +40,6 @@ class Camera:
         self.cancellation_event = cancellation_event
         self.current_capture_source = config.capture_source
         self.wired_camera: "cv2.VideoCapture" = None
-        self.stream = None
-        self.stream_frame_number = 0
-        self.stream_bytes = bytes()
         self.error_message = "Capture source {} not found, retrying"
 
     def set_output_queue(self, camera_output_outgoing: "queue.Queue"):
@@ -52,26 +49,13 @@ class Camera:
         while True:
             if self.cancellation_event.is_set():
                 print("Exiting capture thread")
-                self.cleanup_stream()
                 return
             should_push = True
             # If things aren't open, retry until they are. Don't let read requests come in any earlier
             # than this, otherwise we can deadlock ourselves.
             if (
-                type(self.config.capture_source) == str
-                and "http" in self.config.capture_source
-            ):
-                try:
-                    self.stream = requests.get(self.config.capture_source, stream=True)
-                    self.camera_status = CameraState.CONNECTED
-                except requests.exceptions.RequestException:
-                    print(self.error_message.format(self.config.capture_source))
-            elif (
                 self.config.capture_source != None and self.config.capture_source != ""
             ):
-                # so, they might have switched to a wired camera for some reason, no need to keep the stream running
-                self.cleanup_stream()
-
                 if (
                     self.wired_camera is None
                     or not self.wired_camera.isOpened()
@@ -97,31 +81,10 @@ class Camera:
             if should_push and not self.capture_event.wait(timeout=0.02):
                 continue
 
-            if self.stream:
-                self.get_stream_picture(should_push)
-            else:
-                self.get_wired_camera_picture(should_push)
+            self.get_wired_camera_picture(should_push)
             if not should_push:
                 # if we get all the way down here, consider ourselves connected
                 self.camera_status = CameraState.CONNECTED
-
-    def get_stream_picture(self, should_push):
-        for chunk in self.stream.iter_content(chunk_size=1024):
-            self.stream_bytes += chunk
-            # mjpeg streams are encoded into frames starting and ending with those bytes
-            starting_section = self.stream_bytes.find(b"\xff\xd8")
-            closing_section = self.stream_bytes.find(b"\xff\xd9")
-
-            if starting_section != -1 and closing_section != -1:
-                # we extract the encoded image first, and then prepare the buffer for another frame
-                jpg_data = self.stream_bytes[starting_section : closing_section + 2]
-                self.stream_frame_number += 1
-                self.stream_bytes = self.stream_bytes[closing_section + 2 :]
-                image = cv2.imdecode(
-                    numpy.fromstring(jpg_data, dtype=numpy.uint8), cv2.IMREAD_COLOR
-                )
-                if should_push:
-                    self.push_image_to_queue(image, self.stream_frame_number, 60)
 
     def get_wired_camera_picture(self, should_push):
         try:
@@ -139,12 +102,6 @@ class Camera:
             )
             self.camera_status = CameraState.DISCONNECTED
             pass
-
-    def cleanup_stream(self):
-        if self.stream:
-            self.stream.close()
-            self.stream_bytes = None
-            self.stream_frame_number = 0
 
     def push_image_to_queue(self, image, frame_number, fps):
         # If there's backpressure, just yell. We really shouldn't have this unless we start getting
